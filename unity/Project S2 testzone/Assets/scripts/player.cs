53935653cece4da3ec2144edac4e7b6a8f4643e3
elie ubogi <elie.ubogi@gmail.com>
2019-03-11 12:59:40 +0100

    panel begin

--- unity/Project S2 testzone/Assets/scripts/player.cs
+++ unity/Project S2 testzone/Assets/scripts/player.cs
@@ -29,28 +29,33 @@ public class player : MonoBehaviour
         score = 0;
     }
 
- 
-    
-      void OnCollisionEnter(Collision chose)
+    void OnTriggerEnter(Collider chose)
+    {
+        if (chose.gameObject.tag == ("safe"))
+        {
+            this.resetV();
+            CanGo = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision chose)
     {
         if (chose.gameObject.tag == ("Wall"))
         {
             CanGo = true;
         }
     }
-    
-     void OnCollisionExit(Collision chose)
-     {
-        if (chose.gameObject.tag == ("Wall"))
-        {
-            CanGo = false;
-        }
-     }
+
+    public void resetV()
+    {
+        vitesseX = 0;
+        vitesseY = 0;
+        vitesseZ = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Menu = fin.mainMenu;
         //en cas de probleme :
         if(Input.GetKeyDown(KeyCode.RightShift))
         {
@@ -62,7 +67,7 @@ public class player : MonoBehaviour
             vitesseX = Input.GetAxis("Horizontal");
             vitesseY = Input.GetAxis("Vertical");
             vitesseZ = Input.GetAxis("Depth");
-            if (Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.LeftControl)||Input.GetKeyDown(KeyCode.LeftArrow)||Input.GetKeyDown(KeyCode.RightArrow)||Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.DownArrow))
             {
                 CanGo = false;
                 score++;
@@ -71,20 +76,10 @@ public class player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Menu = true;
+            this.transform.position = safe.transform.position;
         }
-
-        if (Menu)
-        {
-            vitesseX = 0;
-            vitesseY = 0;
-            vitesseZ = 0;
-            Player.transform.position = safe.transform.position;
-            
-        }
-        
         
         rigid.velocity = new Vector3(vitesseX * multiplicateurV * Time.deltaTime,vitesseY * multiplicateurV * Time.deltaTime,vitesseZ * multiplicateurV * Time.deltaTime);
-        
     }
+        
 }
