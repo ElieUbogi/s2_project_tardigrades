2c1648d63c01978a15a759ff8dff518909b79bd8
unknown <victor.litoux@epita.fr>
2019-04-22 18:20:35 +0200

    level_2

---
+++ unity/UnityS2NiveauxAvec1/Assets/scripts/escape_button.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class escape_button : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject panel;
+    public GameObject safe;
+    public List<Button> buttons;
+    private int i = 0;
+
+    private void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // desactivate the cube when we press escape and reactivate it when we repress the button
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (i % 2 == 0)
+            {
+                panel.SetActive(true);
+                player.SetActive(false);
+            }
+            else
+            {
+                panel.SetActive(false);
+                player.SetActive(true);
+            }
+            i++;
+            buttons[0].onClick.AddListener(() => ActionsOnButtons(0));
+        }
+    }
+
+    private void ActionsOnButtons(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                break;
+        }
+    }
+}
