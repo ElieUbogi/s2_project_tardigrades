9ec02a2fc2d00c27d0e98b50ea03014801edead1
ElieUbogi <elie.ubogi@epita.fr>
2019-04-23 10:58:33 +0200

    update

--- unity/Project S2 testzone/Assets/scripts/action_buttons.cs
+++ unity/Project S2 testzone/Assets/scripts/action_buttons.cs
@@ -5,9 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class action_buttons : MonoBehaviour
 {
-    public List<GameObject> buttons;
-
-    // Update is called once per frame
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
