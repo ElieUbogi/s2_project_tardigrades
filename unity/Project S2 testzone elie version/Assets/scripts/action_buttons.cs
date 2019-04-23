9c474e9a60625f9d8d9d084360cd7c1662fee195
Maxime DELCOURT <maxime.delcourt@epita.fr>
2019-04-23 10:53:58 +0200

    tien elie c'etait chiant

---
+++ unity/Project S2 testzone elie version/Assets/scripts/action_buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class action_buttons : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ unity/Project S2 testzone elie version/Assets/scripts/fin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fin : MonoBehaviour
+{
+    public GameObject safe;
+    public GameObject menuPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //safe = GameObject.Find("safe");
+    }
+    void OnTriggerEnter(Collider chose)
+    {
+        if (chose.gameObject.tag == ("Player"))
+        {
+            chose.transform.position = safe.transform.position;
+            menuPanel.SetActive(true);
+            chose.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    void OnTriggerExit(Collider chose)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
