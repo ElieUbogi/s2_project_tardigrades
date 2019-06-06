a84904f63e9a28fd948fe04b58cce20039c6f5ce
unknown <victor.litoux@epita.fr>
2019-06-06 22:59:15 +0200

    Network V6

---
+++ unity/Network final V5/Assets/scripts/TPlorteur.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPlorteur : MonoBehaviour
+{
+    public GameObject Arrivée;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider chose)
+    {
+        if (chose.gameObject.tag == ("Player"))
+        {
+            chose.gameObject.transform.position = Arrivée.transform.position;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ unity/Network final V5/Assets/scripts/UI/optionPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class optionPanel : MonoBehaviour
+{
+    public Dropdown resolutionsDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start() {
+        resolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+
+        List<string> resolutionsName = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            resolutionsName.Add(resolutions[i].width + " x " + resolutions[i].height);
+            
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(resolutionsName);
+        Debug.Log(currentResolutionIndex);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void Setresolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+        // master.SetFloat("volume", volume);
+        AudioListener.volume = volume; 
+    }
+}
